d13868e36775a9a6fdf32d8d40645eaec0eda1b5
Annie Zoyin Lee <annie80113@hotmail.com>
2019-06-08 16:46:48 -0400

    updated letter.js

---
+++ letter.js
@@ -0,0 +1,29 @@
+// Construct for letters
+function Letter(letter) {
+    this.letter = letter;
+    this.userGuess = false;
+
+    // Method that displays the character or an underscore
+    this.display = function() {
+        // If the letter has been guessed, return the letter 
+        if (this.userGuess === true) {
+            return this.letter.toUpperCase();
+        }
+        // If the letter has not been guessed, return an underscore;
+        else {
+            return "_";
+        }  
+    }
+    // Method that check if user input is the same as the letter
+    this.checkLetter = function(userInput) {
+        if (userInput.toLowerCase() === this.letter.toLowerCase()) {
+            this.userGuess = true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+// Export the Letter constructor to word.js
+module.exports = Letter;
